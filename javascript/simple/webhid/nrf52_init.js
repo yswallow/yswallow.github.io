aba688a16ef8bc9151bd3199bb6f7c8fd88480c5
yswallow <1426202+yswallow@users.noreply.github.com>
2021-06-20 01:46:45 +0900

    add webhid page

---
+++ javascript/simple/webhid/nrf52_init.js
@@ -0,0 +1,100 @@
+var device;
+var col_count=0, row_count=0, col_pins=[], row_pins=[];
+
+function two_digit_dec(num) {
+    c = num.toString();
+    if( c.length==1 ) {
+        c = "0"+c;
+    }
+    return c;
+}
+
+function pinName(num) {
+    return `P${parseInt(num/32)}_`+two_digit_dec(num%32);
+}
+function parseHidResponse(event) {
+    const view = new Uint8Array(event.data.buffer);
+    if( view[0] == 0x02 ) {
+        data_ary = [];
+        for(i=2;i<32;i++) {
+            data_ary.push(view[i]);
+        }
+        console.log(data_ary.join(","));
+        if( view[1] == 0x04 ) {
+            col_pins = data_ary.slice();
+        } else if( view[1] == 0x05 ) {
+            row_pins = data_ary.slice();
+        } else if( view[1]== 0x06 ) {
+            col_count = view[3];
+            row_count = view[2];
+        }
+        document.getElementById("received-rows").innerText = row_pins.slice(0,row_count).map(c=>pinName(c)).join(",");
+        document.getElementById("received-cols").innerText = col_pins.slice(0,col_count).map(c=>pinName(c)).join(",");
+    }
+}
+async function Connect(){
+    if("hid" in navigator) {
+        console.log("WebHID API available.");
+        
+        navigator.hid.addEventListener('connect', ({device}) => {
+            console.log(`HID connected: ${device.productName}`);
+        });
+        const filters = [
+            {
+                
+            }
+        ];
+        [device] = await navigator.hid.requestDevice({ filters });
+        
+        device.addEventListener("inputreport", parseHidResponse);
+
+	    console.log(`productName: ${device.productName}`); 
+        for (let collection of device.collections) {
+            console.log(`Usage: ${collection.usage}`);
+            console.log(`UsagePage: ${collection.usagePage}`);
+            for (let inputReport of collection.inputReports) {
+                console.log(`Input report: ${inputReport.reportId}`);
+                // Loop through inputReport.items
+            }
+
+            for (let outputReport of collection.outputReports) {
+                console.log(`Output report: ${outputReport.reportId}`);
+            // Loop through outputReport.items
+            }
+
+            for (let featureReport of collection.featureReports) {
+                console.log(`Feature report: ${featureReport.reportId}`);
+            // Loop through featureReport.items
+            }
+
+            // Loop through subcollections with collection.children
+        }
+        if(! device.opened) {
+            await device.open();
+            console.log( device );
+        }
+        device.sendReport(0x00, new Uint8Array([0x02,0x06]));
+        device.sendReport(0x00, new Uint8Array([0x02,0x04]));
+        device.sendReport(0x00, new Uint8Array([0x02,0x05]));
+        
+    } else {
+        alert("WebHID API not available.");
+    }
+}
+async function send() {
+    rows = document.getElementById("row-text").value.split(",").map(i=>parseInt(i));
+    await device.sendReport(0x00, new Uint8Array( [0x03, 0x05].concat(rows) ));
+    cols = document.getElementById("col-text").value.split(",").map(i=>parseInt(i));
+    await device.sendReport(0x00, new Uint8Array( [0x03, 0x04].concat(cols) ));
+    _row_count = parseInt( document.getElementById("row-count").value );
+    _col_count = parseInt( document.getElementById("col-count").value );
+    await device.sendReport(0x00, new Uint8Array( [0x03, 0x06, _row_count, _col_count] ));
+
+    await device.sendReport(0x00, new Uint8Array([0x02,0x06]));
+    await device.sendReport(0x00, new Uint8Array([0x02,0x04]));
+    await device.sendReport(0x00, new Uint8Array([0x02,0x05]));
+}
+window.addEventListener("load",()=>{
+    document.getElementById("execute").addEventListener("click", Connect);
+    document.getElementById("send").addEventListener("click", send);
+});
