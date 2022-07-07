1593e9ca6bcf1414bf4fbbafea1ad6db8cbdb876
yswallow <1426202+yswallow@users.noreply.github.com>
2022-07-07 19:23:15 +0900

    add VIA keymap layer copy function

--- javascript/simple/webhid/via2keymap.js
+++ javascript/simple/webhid/via2keymap.js
@@ -4,6 +4,7 @@ const keyboardConf = {
     layer_count: 0,
     keymap: [],
     layout: [],
+    layers: [],
 }
 
 function request_layer_count() {
@@ -11,6 +12,8 @@ function request_layer_count() {
 }
 
 function request_keymap() {
+    document.getElementById("in-progress").hidden = false;
+    document.getElementById("finished").hidden = true;
     if( keyboardConf.col_count==0 || keyboardConf.row_count==0 ) {
         document.getElementById("info-file-button").click();
         return;
@@ -31,7 +34,7 @@ function request_keymap() {
         }
     }
 
-    setTimeout(print_keymap, keyboardConf.layer_count*reports_per_layer*100+1000);
+    setTimeout(print_keymap, keyboardConf.layer_count*reports_per_layer*100+200);
 }
 
 function load_infofile(change_ev) {
@@ -86,20 +89,65 @@ function print_keymap() {
     }
 
     for(let l=0; l<keyboardConf.layer_count; l++) {
-        keymap_str += "{";
+        let co_layer_keymap_str = "{";
 
         for(let i=0; i<keyboardConf.layout.length; i++) {
             if( i%keyboardConf.col_count==0 ) {
-                keymap_str += "\r\n";
+                co_layer_keymap_str += "\r\n";
             }
-            keymap_str += getKeyname(keymap[l][i]);
-            keymap_str += ", ";
+            co_layer_keymap_str += getKeyname(keymap[l][i]);
+            co_layer_keymap_str += ", ";
         }
 
-        keymap_str += "\r\n},\r\n";
+        co_layer_keymap_str += "\r\n}";
+        keymap_str += co_layer_keymap_str + ",\r\n";
+        keyboardConf.layers.push(co_layer_keymap_str);
     }
 
     document.getElementById("keymap-area").value = keymap_str;
+    document.getElementById("copy-layer").removeAttribute("disabled");
+    document.getElementById("request-keymap").hidden = false;
+
+    document.getElementById("in-progress").hidden = true;
+    document.getElementById("finished").hidden = false;
+}
+
+function print_co_layer(el) {
+    let id = el.target.getAttribute("id");
+    let target_id = id.replace("input", "keymap");
+    let layer_num = parseInt(el.target.value);
+    
+    document.getElementById(target_id).innerHTML = keyboardConf.layers[layer_num].replaceAll("\r\n", "<br>");
+}
+
+
+function copy_layer() {
+    let copy_from = parseInt( document.getElementById("input-copy-layer-from").value );
+    let copy_to = parseInt( document.getElementById("input-copy-layer-to").value );
+
+    let copy_from_offset = keyboardConf.row_count * keyboardConf.col_count * 2 * copy_from;
+    let copy_to_offset = keyboardConf.row_count * keyboardConf.col_count * 2 * copy_to;
+
+    let len = keyboardConf.row_count*keyboardConf.col_count*2;
+    document.getElementById("copy-layer-finished").hidden = true;
+    for(let i=0;i*28<keyboardConf.row_count*keyboardConf.col_count*2;i++) {
+        let additional_offset = i*28;
+        let to_offset_hi = ( (copy_to_offset + additional_offset) >> 8 ) & 0xFF;
+        let to_offset_lo = ( (copy_to_offset + additional_offset) ) & 0xFF;
+
+        let packet_size = len > 28 ? 28 : len;
+        setTimeout(
+            (data) => {
+                nrf52_common.sendReport(data);
+            },
+            100*i,
+            [0x13, to_offset_hi, to_offset_lo, packet_size].concat(
+                keyboardConf.keymap.slice(copy_from_offset+additional_offset, copy_from_offset+additional_offset+packet_size)
+            )
+        );
+        len -= 28;
+    }
+    setTimeout( ()=>{document.getElementById("copy-layer-finished").hidden=false;}, keyboardConf.row_count*keyboardConf.col_count/14*100);
 }
 
 function set_keymap(offset, data) {
@@ -115,6 +163,8 @@ function parseHidResponse(event) {
         keyboardConf.layer_count = view[1];
         console.log(view[1]);
         setTimeout(request_keymap,100);
+        document.getElementById("input-copy-layer-from").setAttribute("max", view[1]-1);
+        document.getElementById("input-copy-layer-to").setAttribute("max", view[1]-1);
     } else if( view[0] == 0x12) {
         let data_ary = [];
         
@@ -137,7 +187,10 @@ function connectCallback() {
 window.addEventListener("load", function() {
     document.getElementById("info-file-button").addEventListener("click", (ev)=>document.getElementById("info-file-select").click());
     document.getElementById("connect").addEventListener("click", Connect);
-    document.getElementById("print-keymap").addEventListener("click", print_keymap);
+    document.getElementById("request-keymap").addEventListener("click", request_keymap);
     document.getElementById("info-file-select").addEventListener("change", load_infofile);
     document.getElementById("use-alternatives").addEventListener("click", use_alternatives);
+    document.getElementById("copy-layer").addEventListener("click", copy_layer);
+    document.getElementById("input-copy-layer-from").addEventListener("change", print_co_layer);
+    document.getElementById("input-copy-layer-to").addEventListener("change", print_co_layer);
 })
