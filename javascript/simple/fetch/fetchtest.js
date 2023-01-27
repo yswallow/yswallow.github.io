a279e10026cfa94c1a0354034dd6af89696920b3
yswallow <1426202+yswallow@users.noreply.github.com>
2023-01-27 18:05:59 +0900

    add FetchTest

---
+++ javascript/simple/fetch/fetchtest.js
@@ -0,0 +1,15 @@
+function execute_fetch() {
+    let url = document.getElementById("url");
+    fetch(url).then( (res)=>{
+        if(!res.ok) {
+            output_area.innerHTML = "ERROR";
+        } else {
+            output_area.innerHTML = response.text();
+        }
+    });
+}
+
+window.addEventListener("load", function() {
+    const output_area = document.getElementById("output-area");
+    document.getElementById("exec").addEventListener("click", execute_fetch);
+})
